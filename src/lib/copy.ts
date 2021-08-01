0a338c754be76d6a0e266865f43dfd52ecb7b254
su-u <aaaiiiuuu35@gmail.com>
2021-08-01 14:30:23 +0900

    Feat: alert

--- src/lib/copy.ts
+++ src/lib/copy.ts
@@ -1,1 +1,12 @@
-export const copy = (text?: string) => () => text && navigator.clipboard.writeText(text);
+import { Alert } from 'rsuite';
+
+export const copy = (text?: string) => async () => {
+  try {
+    if (text) {
+      await navigator.clipboard.writeText(text);
+      Alert.success('コピーしました。');
+    }
+  } catch (e) {
+    Alert.error('コピー出来ませんでした。');
+  }
+}
