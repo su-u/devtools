9a929e6eebde389c03c12404177fe62a5368edae
su- <happy.oil2469@fastmail.com>
2022-09-19 13:22:58 +0900

    Fix: thema

--- src/components/common/Editor.tsx
+++ src/components/common/Editor.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import MonacoEditor, { EditorProps } from '@monaco-editor/react';
 
 export const Editor: React.VFC<EditorProps> = (props) => {
-  return <MonacoEditor height="50vh" defaultLanguage="Markdown" {...props} />;
+  return <MonacoEditor height="50vh" theme="vs-dark" defaultLanguage="Markdown" {...props} />;
 };
