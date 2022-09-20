e2c34e3c1daeb09def9de85dda913cb9eebb60c3
su- <happy.oil2469@fastmail.com>
2022-09-20 23:26:03 +0900

    Fix: スクロールした際に見やすいように

--- src/components/pages/diff/DiffEditor.tsx
+++ src/components/pages/diff/DiffEditor.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { DiffEditor as MonacoDiffEditor, DiffEditorProps } from '@monaco-editor/react';
 
 export const DiffEditor: React.VFC<DiffEditorProps> = (props) => {
-  return <MonacoDiffEditor height="100vh" theme="vs-dark" {...props} />;
+  return <MonacoDiffEditor height="80vh" theme="vs-dark" {...props} />;
 };
