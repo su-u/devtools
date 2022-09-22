b4723517fb257ed10eafc0bd4109c19d291c6e31
su- <happy.oil2469@fastmail.com>
2022-09-22 13:09:34 +0900

    v3.0.0

---
+++ src/components/pages/urlencode/useUrlEncode.ts
@@ -0,0 +1,38 @@
+import { useForm } from 'react-hook-form';
+import { useEffect, useState } from 'react';
+
+type Base64Form = {
+  input: string;
+};
+
+export const useBase64 = () => {
+  const { control, watch } = useForm<Base64Form>({
+    mode: 'onChange',
+    reValidateMode: 'onChange',
+    criteriaMode: 'firstError',
+    shouldFocusError: true,
+    shouldUnregister: true,
+  });
+  const [output, setOutput] = useState('');
+  const input = watch('input') ?? '';
+
+  useEffect(() => {
+    if (typeof window === 'undefined' || input.trim() === '') {
+      setOutput('');
+      return;
+    }
+
+    const r = new window.FileReader();
+    const blob = new Blob([input]);
+    r.onload = function () {
+      // console.info(r.result.substr(r.result.indexOf(',')+1));
+      setOutput((r.result as string).replace(/data:.*\/.*;base64,/, ''));
+    };
+    r.readAsDataURL(blob);
+  }, [input]);
+
+  return {
+    control,
+    output,
+  };
+};
---
+++ src/pages/urlencode.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Base64 } from '@/components/pages/base64/Base64';
+
+const Base64Page: React.VFC = () => {
+  return <Base64 />;
+};
+
+export default Base64Page;
