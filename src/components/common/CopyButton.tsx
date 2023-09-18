6c2f982e8683f3056e09da8077061ac05b237d7e
su- <30166652+su-u@users.noreply.github.com>
2023-09-18 23:44:57 +0900

    Fix: copy button - base64

---
+++ src/components/common/CopyButton.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from 'react';
+import { IconButton } from 'rsuite';
+import CopyIcon from '@rsuite/icons/Copy';
+import { useCopy } from '@/hooks/useCopy';
+
+type Props = {
+  copyText: string;
+}
+
+export const CopyButton: FC<Props> = ({ copyText }) => {
+  const { copy } = useCopy();
+
+  return (
+    <IconButton
+      icon={<CopyIcon />}
+      placement="right"
+      size="xs"
+      onClick={copy(copyText)}
+    ></IconButton>
+  )
+}
--- src/components/pages/base64/Base64.tsx
+++ src/components/pages/base64/Base64.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import { Controller, FormProvider } from 'react-hook-form';
-import { Col, Grid, Panel, Row, IconButton, ButtonToolbar } from 'rsuite';
+import { Col, Grid, Panel, Row, ButtonToolbar } from 'rsuite';
 import { AppLayout } from '@/Layout/App';
 import { PageTitle } from '@/components/common/PageTitle';
 import { useBase64 } from '@/components/pages/base64/useBase64';
 import { Input } from '@/components/common/Form/Input';
 import { PanelHeader } from '@/components/common/PanelHeader';
-import CopyIcon from '@rsuite/icons/Copy';
-import { useCopy } from '@/hooks/useCopy';
 import { ClearButton } from '@/components/common/Form/ClearButton';
+import { CopyButton } from '@/components/common/CopyButton';
 
 export const Base64: React.FC = () => {
   const title = 'base64エンコード';
   const { methods, output } = useBase64();
-  const { copy } = useCopy();
 
   return (
     <FormProvider {...methods}>
@@ -50,12 +48,7 @@ export const Base64: React.FC = () => {
                     title="base64エンコード"
                     right={
                       <ButtonToolbar>
-                        <IconButton
-                          icon={<CopyIcon />}
-                          placement="right"
-                          size="xs"
-                          onClick={copy(output)}
-                        ></IconButton>
+                        <CopyButton copyText={output} />
                       </ButtonToolbar>
                     }
                   />
