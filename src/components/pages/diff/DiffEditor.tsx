a21d1bab685742f3ef3ad0fb32cd3a187a3dc1df
su- <30166652+su-u@users.noreply.github.com>
2023-09-22 16:24:47 +0900

    Fix: スタイル調整下地

--- src/components/pages/diff/DiffEditor.tsx
+++ src/components/pages/diff/DiffEditor.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import styled from '@emotion/styled';
 import { DiffEditor as MonacoDiffEditor, DiffEditorProps } from '@monaco-editor/react';
 
 export const DiffEditor: React.FC<DiffEditorProps> = (props) => {
-  return <MonacoDiffEditor height="80vh" theme="vs-dark" {...props} />;
+  return (
+    <StyledWrapper>
+      <MonacoDiffEditor className="diff-editor" height="80vh" theme="vs-dark" {...props} />
+    </StyledWrapper>);
 };
+
+const StyledWrapper = styled.div``;
