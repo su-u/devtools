8e52b2833d6ff4a0e1ed2af6fb49d116cc7cfb6f
su- <30166652+su-u@users.noreply.github.com>
2023-12-03 22:31:46 +0900

    Refactor: CharacterCount

--- src/app/character_count/page.tsx
+++ src/app/character_count/page.tsx
@@ -1,8 +1,98 @@
+'use client';
+import styled from '@emotion/styled';
 import React from 'react';
-import { CharacterCount } from '@/components/pages/character_count/CharacterCount';
+import { Controller, FormProvider } from 'react-hook-form';
+import { Col, Form, Grid, Panel, Row, ButtonToolbar } from 'rsuite';
+import { AppLayout } from '@/Layout/App';
+import { ClearButton } from '@/components/common/Form/ClearButton';
+import { Input } from '@/components/common/Form/Input';
+import { LabelInput } from '@/components/common/Form/LabelInput';
+import { useCustomForm } from '@/components/common/Form/useCustomForm';
+import { PageTitle } from '@/components/common/PageTitle';
+import { PanelHeader } from '@/components/common/PanelHeader';
+import {
+  characterCountWithoutSpace,
+  characterCountWithSpace,
+  fullWidthCharacterCount,
+  halfWidthCharacterCount,
+  linesCount,
+  spaceCount,
+} from '@/app/character_count/CharacterCountLib';
+
+type characterCountForm = {
+  input: string;
+};
 
 const CharacterCountPage: React.FC = () => {
-  return <CharacterCount />;
+  const methods = useCustomForm<characterCountForm>({
+    defaultValues: {
+      input: '',
+    },
+  });
+  const { control, watch } = methods;
+
+  const title = '文字数カウント';
+  const input = watch('input', '');
+
+  const characterCountValue = characterCountWithSpace(input).toString();
+  const characterCountWithoutSpaceValue = characterCountWithoutSpace(input).toString();
+  const spaceCharacterCountValue = spaceCount(input).toString();
+  const fullWidthCharacterCountValue = fullWidthCharacterCount(input).toString();
+  const halfWidthCharacterCountValue = halfWidthCharacterCount(input).toString();
+  const linesCountValue = linesCount(input).toString();
+
+  return (
+    <FormProvider {...methods}>
+      <AppLayout title={title}>
+        <Grid fluid>
+          <PageTitle title={title} />
+          <Row gutter={5}>
+            <Col xs={24} md={12}>
+              <Panel
+                bordered
+                header={
+                  <PanelHeader
+                    title="カウントする文字列"
+                    right={
+                      <ButtonToolbar>
+                        <ClearButton name="input" />
+                      </ButtonToolbar>
+                    }
+                  />
+                }
+              >
+                <Controller
+                  render={({ field }) => (
+                    <Input noResize="none" as="textarea" rows={14} {...field} ref={null} />
+                  )}
+                  name="input"
+                  control={control}
+                />
+              </Panel>
+            </Col>
+            <Col xs={24} md={12}>
+              <Panel bordered header={<PanelHeader title="文字数" />}>
+                <ConvertedForm layout="horizontal">
+                  <LabelInput label="文字数(スペース込み)" value={characterCountValue} />
+                  <LabelInput label="文字数(スペース除)" value={characterCountWithoutSpaceValue} />
+                  <LabelInput label="スペースの数" value={spaceCharacterCountValue} />
+                  <LabelInput label="全角文字数" value={fullWidthCharacterCountValue} />
+                  <LabelInput label="半角文字数" value={halfWidthCharacterCountValue} />
+                  <LabelInput label="行数" value={linesCountValue} />
+                </ConvertedForm>
+              </Panel>
+            </Col>
+          </Row>
+        </Grid>
+      </AppLayout>
+    </FormProvider>
+  );
 };
+
+const ConvertedForm = styled(Form)`
+  > div:not(:last-child) {
+    margin-bottom: 12px !important;
+  }
+`;
 
 export default CharacterCountPage;
