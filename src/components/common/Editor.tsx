f9e32cf45595fc5167e685e1a623f28597024e74
su- <30166652+su-u@users.noreply.github.com>
2023-12-04 21:49:58 +0900

    Fix: エディターをcodemirrorに

--- src/components/common/Editor.tsx
+++ src/components/common/Editor.tsx
@@ -1,6 +1,8 @@
-import MonacoEditor, { EditorProps } from '@monaco-editor/react';
+import { vscodeDark } from '@uiw/codemirror-theme-vscode';
+import CodeMirror from '@uiw/react-codemirror';
+import { ReactCodeMirrorProps } from '@uiw/react-codemirror/src';
 import React from 'react';
 
-export const Editor: React.FC<EditorProps> = (props) => {
-  return <MonacoEditor height="50vh" theme="vs-dark" defaultLanguage="Markdown" {...props} />;
+export const Editor: React.FC<ReactCodeMirrorProps> = (props) => {
+  return <CodeMirror height="50vh" theme={vscodeDark} {...props} />;
 };
