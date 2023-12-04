a1f85442b82bc2eab249d40c4d07f3a5c89ca779
su- <30166652+su-u@users.noreply.github.com>
2023-12-04 22:23:54 +0900

    Fix: 不要なコード削除

--- src/app/character_count/page.tsx
+++ src/app/character_count/page.tsx
@@ -31,13 +31,6 @@ const CharacterCountPage: React.FC = () => {
   });
   const { control, watch } = methods;
 
-  const onChange = useCallback(
-    (value: string) => {
-      methods.setValue('input', value);
-    },
-    [methods],
-  );
-
   const title = '文字数カウント';
   const input = watch('input', '');
 
@@ -69,7 +62,7 @@ const CharacterCountPage: React.FC = () => {
                 }
               >
                 <Controller
-                  render={({ field }) => <Editor value={input} onChange={onChange} {...field} />}
+                  render={({ field }) => <Editor {...field} />}
                   name="input"
                   control={control}
                 />
