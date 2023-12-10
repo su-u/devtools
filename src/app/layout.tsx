724db412971080fbfd757b52ad44300e4f7bbce6
su- <30166652+su-u@users.noreply.github.com>
2023-12-11 00:42:03 +0900

    Fix: metadaの指定方法を修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Analytics } from '@vercel/analytics/react';
 import Head from 'next/head';
+import type { Metadata } from 'next';
 import React from 'react';
 import 'rsuite/dist/rsuite.min.css';
 import '@/styles/globals.scss';
@@ -7,10 +8,12 @@ import '@/styles/rs-custom.globals.scss';
 import { Provider } from '@/app/Provider';
 import GoogleAnalytics from '@/components/common/GoogleAnalytics';
 
-const METADATA = {
+export const metadata: Metadata = {
   title: 'Dev Toolkit',
   description: 'Web utility tools for developers',
-  author: 'su-u',
+  authors: {
+    name: 'su-u',
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -20,13 +23,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ja">
       <Head>
-        <title>{METADATA.title}</title>
         <meta
           name="google-site-verification"
           content="XbfQBUU8iwwFG0-q5UtR_Rde1UfvDzdspEHt2tPs-Uw"
         />
-        <meta name="description" content={METADATA.description} />
-        <meta name="author" content={METADATA.author} />
         <GoogleAnalytics />
       </Head>
       <body className={className}>
